d09b26afd949deb403d50eda924dfb7d570293c4
Jules <julie.luu.jl@googlemail.com>
2021-02-16 19:41:17 +0000

    completed task 3.2.2

---
+++ module-3/lesson-2/scripts/task-3.2.2.js
@@ -0,0 +1,11 @@
+function combineName(firstName, lastName){
+    let fullName = firstName + ' ' + lastName;
+    console.log(fullName);
+}
+
+var fName = 'Jules';
+var lName = 'Luu'
+
+combineName(fName, lName);
+combineName('Julie','Luu');
+
