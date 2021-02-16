9e96935b3bb06b4ebb4634938d14dfea2f37c027
Jules <julie.luu.jl@googlemail.com>
2021-02-16 20:11:42 +0000

    completed task 3.2.4

---
+++ module-3/lesson-2/scripts/task-3.2.4.js
@@ -0,0 +1,12 @@
+var temp = 18;
+
+function wearACoat(temperature) {
+    if(temperature < 10) {
+        console.log('Wrap up warm!');
+    }
+}
+
+wearACoat(12);
+wearACoat(10);
+wearACoat(8);
+wearACoat(5);
