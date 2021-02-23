fedce21c826dadfb36c29af2e18cf77d76edc285
Jules <julie.luu.jl@googlemail.com>
2021-02-23 20:12:10 +0000

    completed task 3.4.3

---
+++ module-3/lesson-4/scripts/task-3.4.3.js
@@ -0,0 +1,6 @@
+var arrFaveFoods = ['sushi', 'ramen', 'mochi'];
+
+for (var counter = 0; counter < arrFaveFoods.length; counter++){
+    let food = arrFaveFoods[counter];
+    console.log(counter + ': ' + food);
+}
