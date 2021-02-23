35f7f9fd753513a7b5e3075d2755f3906fbe220a
Jules <julie.luu.jl@googlemail.com>
2021-02-23 20:26:29 +0000

    completed task 3.4.4

---
+++ module-3/lesson-4/scripts/task-3.4.4.js
@@ -0,0 +1,29 @@
+var objRecipe = {
+    recipeTitle: 'Omelette',
+    servings: 1,
+    ingredients: [
+        'Two Eggs',
+        'Mushrooms',
+        'Ham'
+    ],
+    directions: [
+        'Break two eggs, and beat the eggs',
+        'Heat up a pan and add a little bit of oil',
+        'Pour the eggs mix into the pan on low heat and wait for a few minutes',
+        'Add mushrooms and ham and fold the omelette into half',
+        'Cook both side for few minutes and serve'
+    ]
+};
+
+console.log(objRecipe.recipeTitle);
+console.log(objRecipe.servings);
+
+for (var counter = 0; counter < objRecipe.ingredients.length; counter++){
+    let ingredient = objRecipe.ingredients[counter];
+    console.log(ingredient);
+}
+
+for (var counter = 0; counter < objRecipe.directions.length; counter++){
+    let direction = objRecipe.directions[counter];
+    console.log(direction);
+}
