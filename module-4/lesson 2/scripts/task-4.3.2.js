56fb300840a9752da453b1570f40b0328f6baecb
Jules <julie.luu.jl@googlemail.com>
2021-03-11 21:26:02 +0000

    completed task 4.3.2
    wrote out peusdo code for fixStart and the code for it

---
+++ module-4/lesson 2/scripts/task-4.3.2.js
@@ -0,0 +1,39 @@
+// FUNCTION name FixStart(string)
+function fixStart(string){
+    //SET {firstCharacter} equal string[0];
+    var firstCharacter = string[0].toLowerCase();
+    console.log(firstCharacter);
+    //SET {newString} equal  "";
+    var newString = "";
+    //LOOP through each character of string
+    for (counter = 0; counter < string.length; counter++){
+        var currentCharacter = string[counter].toLowerCase();
+        //IF {newString} is empty;
+        if(!newString){
+            //THEN {newString} equal {firstCharacter};
+            newString = currentCharacter;
+        }   
+        //ELSE
+        else {
+            //IF current character equals {firstCharacter}
+            if(currentCharacter == firstCharacter){
+                //THEN newString += *
+                newString += '*';
+            }
+        // ELSE 
+        else {
+            // THEN newString += current character
+            newString += currentCharacter;
+            }
+        }
+    }
+
+    return newString;
+}
+
+var string = fixStart('bubble');
+console.log(string);
+string = fixStart('alexander');
+console.log(string);
+string = fixStart('goggle');
+console.log(string);
